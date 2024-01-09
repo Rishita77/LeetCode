e99b739be57b43c4d55732b903fa43469c127c4b
Rishita77 <rishita.m8@gmail.com>
2024-01-09 10:51:15 +0530

    changes to combSum

--- Valid_Parentheses_20.java
+++ Valid_Parentheses_20.java
@@ -1,69 +1,48 @@
 import java.util.Stack;
-
 public class Valid_Parentheses_20 {
 
-    public static boolean isValid(String s) {
+//    public class Solution {
 
-        if(s.length() == 0 || s.length() == 1){
-            return false;
-        }
-
-        Stack<Character> st = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-
-            char c = s.charAt(i);
-
-            if(c == '(' || c == '{' || c == '['){
-                st.push(c);
+        public static boolean isValid(String s) {
+            if (s.length() == 0 || s.length() == 1) {
+                return false;
             }
 
-            else if(c == ')'){
-                while(!st.empty()){
-                    if(st.peek() == '('){
-                        break;
+            Stack<Character> st = new Stack<>();
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if (c == '(' || c == '{' || c == '[') {
+                    st.push(c);
+                } else if (c == ')') {
+                    if (!st.isEmpty() && st.peek() == '(') {
+                        st.pop();
+                    } else {
+                        return false; // Mismatched closing bracket
                     }
-                    st.pop();
+                } else if (c == '}') {
+                    if (!st.isEmpty() && st.peek() == '{') {
+                        st.pop();
+                    } else {
+                        return false; // Mismatched closing bracket
+                    }
+                } else if (c == ']') {
+                    if (!st.isEmpty() && st.peek() == '[') {
+                        st.pop();
+                    } else {
+                        return false; // Mismatched closing bracket
+                    }
                 }
             }
 
-            else if(c == '}'){
-                while(!st.empty()){
-                    if(st.peek() == '{'){
-                        break;
-                    }
-                    st.pop();
-                }
-            }
-
-            else if(c == ']'){
-                while(!st.empty()){
-                    if(st.peek() == '['){
-                        break;
-                    }
-                    st.pop();
-                }
-            }
-
-        }
-
-        if(!st.empty()){
-
-            return false;
-
+            return st.isEmpty();
         }
 
 
-        return true;
-
-
-
+        public static void main(String[] args) {
+            String st = "()({[}])";
+            System.out.println(isValid(st));
+        }
     }
-
-    public static void main(String[] args) {
-
-        String st = "()(";
-        System.out.println(isValid(st));
-
-    }
-}
+//}
